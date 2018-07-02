4ba35a5a652ecf0e533c775ac88cac3daeaa5da9
will-rice <will-rice@github.com>
2018-07-02 16:19:09 -0400

    added general Conv1D layer with the option to be causal

---
+++ tensorflow/layers.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+
+class Conv1D(tf.layers.Conv1D):
+    def __init__(self, filters,
+               kernel_size,
+               strides=1,
+               dilation_rate=1,
+               activation=None,
+               use_bias=True,
+               kernel_initializer=None,
+               bias_initializer=tf.zeros_initializer(),
+               kernel_regularizer=None,
+               bias_regularizer=None,
+               activity_regularizer=None,
+               kernel_constraint=None,
+               bias_constraint=None,
+               trainable=True,
+               name=None,
+               is_causal=False
+               **kwargs):
+        super(Conv1D, self).__init__(
+            filters=filters,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding='valid',
+            data_format='channels_last',
+            dilation_rate=dilation_rate,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            kernel_constraint=kernel_constraint,
+            bias_constraint=bias_constraint,
+            trainable=trainable,
+            name=name, **kwargs
+        )
+       
+    def call(self, inputs):
+        if self.is_causal:
+            padding = (self.kernel_size[0] - 1) * self.dilation_rate[0]
+            inputs = tf.pad(inputs, tf.constant([(0, 0,), (1, 0), (0, 0)]) * padding)
+        return super(Conv1D, self).call(inputs)
+        
