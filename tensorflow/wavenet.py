e6493ba9bdd9ff8bb5a99574ba2cf4dd7543eac4
will-rice <will-rice@github.com>
2018-07-06 14:23:15 -0400

    layer checkpoint

--- tensorflow/wavenet.py
+++ tensorflow/wavenet.py
@@ -5,47 +5,52 @@ from __future__ import print_function
 # Imports
 import tensorflow as tf
 import math
-from tensorflow.layers import conv1d
+from layers import CausualConv1D
 
 
-def create_variable(name, shape):
-    '''Create a convolution filter variable with the specified name and shape,
-    and initialize it using Xavier initialition.'''
-    initializer = tf.contrib.layers.xavier_initializer_conv2d()
-    variable = tf.Variable(initializer(shape=shape), name=name)
-    return variable
 
-class WaveNet(object):
+
+class WaveNet(tf.layers.Layer):
     """"""
     def __init__(self, n_in_channels, n_layers, max_dilation,
                  n_residual_channels, n_skip_channels, n_out_channels,
-                 n_cond_channels, upsamp_window, upsamp_stride):
+                 n_cond_channels, upsamp_window, upsamp_stride,
+                 trainable=True, **kwargs):
 
-                self.upsample = tf.layers.Conv2dTranspose()
+                super(WaveNet, self).__init__(
+                    trainable=trainable, 
+                    activity_regularizer=activity_regularizer,
+                    name=name, **kwargs
+                )
+                
                 self.n_layers = n_layers
                 self.max_dilation = max_dilation
                 self.n_residual_channels = n_residual_channels
                 self.n_out_channels = n_out_channels
-                self.cond_layers = conv1d(inputs=,
+                self.upsample = 
+                self.cond_layers = tf.layers.Conv1D(inputs=,
                                           filters=,
-                                          kernel_size=,
-                                          )
-                self.dilate_layers = tf.contrib.layers.stack(inputs,
-                                                             conv1d,
-
-                                                             )
-                self.res_layers = tf.contrib.layers.stack(inputs,
-                                                          conv1d,
-                                                          )
-                self.skip_layers = tf.contrib.layers.stack()
-
+                                          kernel_size=)
+                self.dilate_layers = []
+                self.res_layers = []
+                self.skip_layers = []
                 self.embed = tf.get_variable("embeddings",
                                              [n_in_channels,
                                               n_residual_channels])
-                self.conv_out = conv1d()
-                self.conv_end = conv1d()
-                self.variables = self._create_variables()
+                self.conv_out = tf.layers.Conv1D()
+                self.conv_end = tf.layers.Conv1D()
 
+                loop_factor = math.floor(math.log2(max_dilation)) + 1
+                for i in range(n_layers):
+                    dilation = 2 ** (i % loop_factor)
 
+                    # Kernel size is 2 for nv-wavenet
+                    in_layer = CausualConv1D(n_residual_channels, n_residual_channels,
+                                             kernel_size=2, dilation=dilation,
+                                             activation=tf.nn.tanh)
+                    self.dilate_layers.append(in_layer)
+            
 
+            def call(self, inputs, training=True):
+                
 
