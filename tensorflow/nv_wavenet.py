cd427d4dc0bf33ead6447f496c75e79e71a71058
will-rice <will-rice@github.com>
2018-07-16 13:13:35 -0400

    converted torch functions to numpy equivalent

--- tensorflow/nv_wavenet.py
+++ tensorflow/nv_wavenet.py
@@ -24,7 +24,8 @@
 #  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # 
 # *****************************************************************************
-import torch
+import tensorflow as tf
+import numpy as np
 import nv_wavenet_ext
 
 def interleave_lists(a, b, c, d, e, f, g):
@@ -35,18 +36,18 @@ def column_major(x):
     PyTorch Tensors are row major, so this just returns a contiguous transpose
     """
     assert(x.is_contiguous)
-    if len(x.size()) == 1:
+    if len(x.shape) == 1:
         return x
     
-    if len(x.size()) == 3:
+    if len(x.shape) == 3:
         assert(x.size(2)==1)
-        x = torch.squeeze(x)
+        x = np.squeeze(x)
     
-    if len(x.size())==2:
-        return torch.t(x).contiguous()
+    if len(x.shape)==2:
+        return np.t(x).ascontiguousarray()
     
-    if len(x.size())==4:
-        return x.permute(3,2,1,0).contiguous()
+    if len(x.shape)==4:
+        return x.permute(3,2,1,0).ascontiguousarray()
 
 def enum(**enums):
     return type('Enum', (), enums)
@@ -71,38 +72,38 @@ class NVWaveNet:
 
         self.max_dilation = max_dilation
         self.use_embed_tanh = use_embed_tanh
-        assert embedding_prev.size() == (self.A, self.R), \
+        assert embedding_prev.shape == (self.A, self.R), \
                 ("embedding_prev: {} doesn't match compiled" 
-                " nv-wavenet size: {}").format(embedding_prev.size(),
+                " nv-wavenet size: {}").format(embedding_prev.shape,
                                                (self.A, self.R))
-        self.embedding_prev = column_major(torch.t(embedding_prev))
+        self.embedding_prev = column_major(np.T(embedding_prev))
         
-        assert embedding_curr.size() == (self.A, self.R), \
+        assert embedding_curr.shape == (self.A, self.R), \
                 ("embedding_curr: {} doesn't match compiled" 
-                " nv-wavenet size: {}").format(embedding_curr.size(),
+                " nv-wavenet size: {}").format(embedding_curr.shape,
                                                (self.A, self.R))
-        self.embedding_curr = column_major(torch.t(embedding_curr))
+        self.embedding_curr = column_major(np.T(embedding_curr))
         
-        assert conv_out_weight.size()[:2] == (self.A, self.S), \
+        assert conv_out_weight.shape[:2] == (self.A, self.S), \
                 ("conv_out_weight: {} doesn't match compiled" 
-                 " nv-wavenet size: {}").format(conv_out_weight.size()[:2],
+                 " nv-wavenet size: {}").format(conv_out_weight.shape[:2],
                                                (self.A, self.S))
         self.conv_out = column_major(conv_out_weight)
         
-        assert conv_end_weight.size()[:2] == (self.A, self.A), \
+        assert conv_end_weight.shape[:2] == (self.A, self.A), \
                 ("conv_end_weight: {} doesn't match compiled" 
-                 " nv-wavenet size: {}").format(conv_end_weight.size()[:2],
+                 " nv-wavenet size: {}").format(conv_end_weight.shape[:2],
                                                (self.A, self.A))
         self.conv_end = column_major(conv_end_weight)
         
         dilate_weights_prev = [] 
         dilate_weights_curr = [] 
         for weight in dilate_weights:
-            assert weight.size(2) == 2, \
+            assert weight.shape[2] == 2, \
                     "nv-wavenet only supports kernel_size 2"
-            assert weight.size()[:2] == (2*self.R, self.R), \
+            assert weight.shape[:2] == (2*self.R, self.R), \
                     ("dilated weight: {} doesn't match compiled" 
-                     " nv-wavenet size: {}").format(weight.size()[:2],
+                     " nv-wavenet size: {}").format(weight.shape[:2],
                                                    (2*self.R, self.R))
             Wprev = column_major(weight[:,:,0])
             Wcurr = column_major(weight[:,:,1])
@@ -110,25 +111,25 @@ class NVWaveNet:
             dilate_weights_curr.append(Wcurr)
         
         for bias in dilate_biases:
-            assert(bias.size(0) == 2*self.R)
+            assert(bias.shape[0] == 2*self.R)
         for weight in res_weights:
-            assert weight.size()[:2] == (self.R, self.R), \
+            assert weight.shape[:2] == (self.R, self.R), \
                     ("residual weight: {} doesn't match compiled" 
-                     " nv-wavenet size: {}").format(weight.size()[:2],
+                     " nv-wavenet size: {}").format(weight.shape[:2],
                                                    (self.R, self.R))
         for bias in res_biases:
-            assert(bias.size(0) == self.R), \
+            assert(bias.shape[0] == self.R), \
                     ("residual bias: {} doesn't match compiled" 
-                     " nv-wavenet size: {}").format(bias.size(0), self.R)
+                     " nv-wavenet size: {}").format(bias.shape[0], self.R)
         for weight in skip_weights:
-            assert weight.size()[:2] == (self.S, self.R), \
+            assert weight.shape[:2] == (self.S, self.R), \
                     ("skip weight: {} doesn't match compiled" 
-                     " nv-wavenet size: {}").format(weight.size()[:2],
+                     " nv-wavenet size: {}").format(weight.shape[:2],
                                                    (self.S, self.R))
         for bias in skip_biases:
-            assert(bias.size(0) == self.S), \
+            assert(bias.shape[0] == self.S), \
                     ("skip bias: {} doesn't match compiled" 
-                     " nv-wavenet size: {}").format(bias.size(0), self.S)
+                     " nv-wavenet size: {}").format(bias.shape[0], self.S)
         
         dilate_biases = [column_major(bias) for bias in dilate_biases]
         res_weights = [column_major(weight) for weight in res_weights]
@@ -137,8 +138,8 @@ class NVWaveNet:
         skip_biases = [column_major(bias) for bias in skip_biases]
 
         # There's an extra residual layer that's not used
-        res_weights.append(torch.zeros(self.R,self.R))
-        res_biases.append(torch.zeros(self.R))
+        res_weights.append(np.zeros(self.R,self.R))
+        res_biases.append(np.zeros(self.R))
 
         assert(len(res_biases)==len(skip_biases) and 
         len(res_biases)==len(dilate_biases) and 
@@ -171,15 +172,15 @@ class NVWaveNet:
 
     def infer(self, cond_input, implementation):
         # cond_input is channels x batch x num_layers x samples
-        assert(cond_input.size()[0:3:2] == (2*self.R, self.num_layers)), \
+        assert(cond_input.shape[0:3:2] == (2*self.R, self.num_layers)), \
         """Inputs are channels x batch x num_layers x samples.
         Channels and num_layers should be sizes: {}
         But input is: {}""".format((2*self.R, self.num_layers),
-                                    cond_input.size()[0:3:2])
-        batch_size = cond_input.size(1)
-        sample_count = cond_input.size(3)
+                                    cond_input.shape[0:3:2])
+        batch_size = cond_input.shape[1]
+        sample_count = cond_input.shape[3]
         cond_input = column_major(cond_input)
-        samples = torch.cuda.IntTensor(batch_size, sample_count)
+        samples = np.array(batch_size, sample_count)
         nv_wavenet_ext.infer(samples,
                              sample_count,
                              batch_size,
